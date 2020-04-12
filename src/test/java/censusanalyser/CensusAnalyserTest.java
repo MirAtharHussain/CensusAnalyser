392f303a56b7e647fa925c7b4dcd1ecd4a1770b7
MirAtharHussain <miratharhussain52@gmail.com>
2020-04-13 02:44:38 +0530

    [Hussain]ADDED:If File path is wrong then throws an exception

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -10,7 +10,8 @@ public class CensusAnalyserTest {
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String TYPE_INCORRECT_FILE="./src/test/resources/IndiaStateCensusData.pdf";
     private static final String DElIMITER_HEADER_INCORRECT="./src/test/resources/StateCensusData.csv";
-    private static final String STATE_CODE_CSV_FILE_PATH = "src/test/resources/IndiaStateCode.csv";
+    private static final String STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
+    private static final String STATE_CODE_CSV_WRONG_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
 
 
     @Test
@@ -71,6 +72,16 @@ public class CensusAnalyserTest {
         try {
              numOfRecords = censusAnalyser.loadIndianStateCodeData(STATE_CODE_CSV_FILE_PATH);
             Assert.assertEquals(37,numOfRecords);
+        } catch (CensusAnalyserException e) { }
+    }
+
+    @Test
+    public void givenStateCodeCSVFile_WhenWrongFile_ReturnException() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndianStateCodeData(STATE_CODE_CSV_WRONG_FILE_PATH);
         } catch (CensusAnalyserException e) { }
     }
 }
