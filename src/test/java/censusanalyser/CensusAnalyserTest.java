2060359c68d0cb63a4e1039b98dd6ec3323f9403
MirAtharHussain <miratharhussain52@gmail.com>
2020-04-12 13:04:49 +0530

    [Hussain]ADDED:Checking whether the given file is csv or other format

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -8,6 +8,7 @@ public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String TYPE_INCORRECT_FILE="./src/test/resources/IndiaStateCensusData.pdf";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -26,7 +27,19 @@ public class CensusAnalyserTest {
             exceptionRule.expect(CensusAnalyserException.class);
             censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
-            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+
         }
+    }
+
+    @Test
+    public void givenStateCensusCSVFile_TypeIncorrect_ReturnsCustomException() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        try {
+            censusAnalyser.loadIndiaCensusData(TYPE_INCORRECT_FILE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.TYPE_EXTENSION_WRONG,e.type);
+        }
+
+
     }
 }
