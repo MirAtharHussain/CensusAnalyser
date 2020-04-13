5db5b652a7a4c13f0de9b8aeecb0d4d98a1cc838
MirAtharHussain <miratharhussain52@gmail.com>
2020-04-13 20:03:08 +0530

    [Hussain]Refactor5:Refactored code to take in List of census data instead of iterating through the file

--- src/main/java/censusanalyser/OpenCsvBuilder.java
+++ src/main/java/censusanalyser/OpenCsvBuilder.java
@@ -5,21 +5,31 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 
 public  class OpenCsvBuilder<E> implements ICSVBuilder{
 
     @Override
     public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException {
-        try {
-            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+       return this.getCSVBean(reader, csvClass).iterator();
+    }
+
+    private CsvToBean<E> getCSVBean(Reader reader, Class csvClass) throws  CSVBuilderException{
+        try{
+            CsvToBeanBuilder<E> csvToBeanBuilder=new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(csvClass);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            return csvToBean.iterator();
+            CsvToBean<E> csvToBean=csvToBeanBuilder.build();
+            return  csvToBeanBuilder.build();
         } catch (IllegalStateException e) {
-           throw new CSVBuilderException(e.getMessage(),CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);
+            throw new CSVBuilderException(e.getMessage(),CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);
         }
+    }
+
+    @Override
+    public List getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException {
+        return this.getCSVBean(reader, csvClass).parse();
     }
 }
 
