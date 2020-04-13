8af3fc7bb7a477c943cc7e8e31139ef396b09b45
MirAtharHussain <miratharhussain52@gmail.com>
2020-04-13 23:07:30 +0530

    [Hussain]ADDED:Created Dao object to encapsulated the variable

---
+++ src/main/java/censusanalyser/IndiaCensusDAO.java
@@ -0,0 +1,17 @@
+package censusanalyser;
+
+public class IndiaCensusDAO {
+
+    public int population;
+    public int densityPerSqKm;
+    public int areaInSqkm;
+    public String state;
+
+    public IndiaCensusDAO(IndiaCensusCSV indiaCensusCSV) {
+        this.state=indiaCensusCSV.state;
+        this.areaInSqkm=indiaCensusCSV.areaInSqKm;
+        this.densityPerSqKm=indiaCensusCSV.densityPerSqKm;
+        this.population=indiaCensusCSV.population;
+
+    }
+}
